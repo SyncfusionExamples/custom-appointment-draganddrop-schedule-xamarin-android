db86366457233aff818d7a2394cfb71b303d1b48
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-02-28 16:03:03 +0530

    XAMARINANDROID-3333  - Sample added.

---
+++ ScheduleXamarin/ScheduleXamarin/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Com.Syncfusion.Schedule;
+using Com.Syncfusion.Schedule.Enums;
+using Java.Util;
+using Android.Graphics;
+using System.Collections.ObjectModel;
+
+namespace ScheduleXamarin
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        SfSchedule schedule;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            schedule = new SfSchedule(this);
+            schedule.ScheduleView = ScheduleView.WeekView;
+            schedule.AllowAppointmentDrag = true;
+
+            AppointmentMapping dataMapping = new AppointmentMapping();
+            dataMapping.Subject = "EventName";
+            dataMapping.StartTime = "From";
+            dataMapping.EndTime = "To";
+            dataMapping.Color = "Color";
+            dataMapping.IsAllDay = "IsAllDay";
+            schedule.AppointmentMapping = dataMapping;
+
+            this.AddAppointments();
+            SetContentView(schedule);
+        }
+
+        /// <summary>
+        /// Add appointments into schedule ItemsSource
+        /// </summary>
+        private void AddAppointments()
+        {
+            var Meetings = new ObservableCollection<Meeting>();
+            Calendar currentDate = Calendar.Instance;
+            Calendar startTime = (Calendar)currentDate.Clone();
+
+            startTime.Set(currentDate.Get(CalendarField.Year),
+                          currentDate.Get(CalendarField.Month),
+                          currentDate.Get(CalendarField.DayOfMonth),
+                          10, 0, 0);
+
+            Calendar endTime = (Calendar)currentDate.Clone();
+
+            endTime.Set(currentDate.Get(CalendarField.Year),
+                        currentDate.Get(CalendarField.Month),
+                        currentDate.Get(CalendarField.DayOfMonth),
+                        11, 0, 0);
+
+            var meeting = new Meeting();
+            meeting.From = startTime;
+            meeting.To = endTime;
+            meeting.EventName = "Meeting";
+            meeting.Color = Color.Red;
+            Meetings.Add(meeting);
+
+            var allDay = new Meeting();
+            allDay.From = startTime;
+            allDay.To = endTime;
+            allDay.EventName = "All day Meeting";
+            allDay.Color = Color.GreenYellow;
+            allDay.IsAllDay = true;
+            Meetings.Add(allDay);
+
+            schedule.ItemsSource = Meetings;
+        }
+    }
+}
---
+++ ScheduleXamarin/ScheduleXamarin/Model/Meeting.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+
+namespace ScheduleXamarin
+{
+    public class Meeting : INotifyPropertyChanged
+    {
+        private string eventname;
+        private Calendar from;
+        private Calendar to;
+        private bool isAllDay;
+        private int color;
+
+        public string EventName
+        {
+            get { return eventname; }
+            set
+            {
+                eventname = value;
+                this.RaisePropertyChanged("EventName");
+            }
+        }
+
+        public Calendar From
+        {
+            get { return from; }
+            set
+            {
+                from = value;
+                this.RaisePropertyChanged("From");
+            }
+        }
+
+        public Calendar To
+        {
+            get { return to; }
+            set
+            {
+                to = value;
+                this.RaisePropertyChanged("To");
+            }
+        }
+
+        public bool IsAllDay
+        {
+            get { return isAllDay; }
+            set
+            {
+                isAllDay = value;
+                this.RaisePropertyChanged("IsAllDay");
+            }
+        }
+
+        public int Color
+        {
+            get { return color; }
+            set
+            {
+                color = value;
+                this.RaisePropertyChanged("Color");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(String property)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
